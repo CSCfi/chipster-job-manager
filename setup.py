f67ec739df936df16d2b455dba2335a1fa58a123
Harri Hämäläinen <hhamalai@iki.fi>
2015-04-09 10:20:29 +0300

    migrated from PBR to distutils

--- setup.py
+++ setup.py
@@ -1,2 +1,16 @@
-import setuptools
-setuptools.setup(setup_requires=['pbr'], pbr=True)
+# coding=utf-8
+from distutils.core import setup
+setup(
+    name="Chipster Job Manager",
+    version='3.0',
+    description='Manages long running jobs on Chipster platform',
+    license='MIT'
+    author='Harri Hämäläinen',
+    author_email='harri.hamalainen@csc.fi',
+    packages=['jobmanager'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ])
